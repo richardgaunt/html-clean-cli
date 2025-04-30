438f8c40906dded85d058b68e61a9adbc2c46e9f
rgaunt <richard.gaunt@protonmail.com>
2025-04-30 13:24:14 +1000

    Updated jest config.

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  testMatch: ['**/test/**/*.test.js'],
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov'],
-  collectCoverageFrom: ['src/**/*.js'],
-  setupFilesAfterEnv: ['<rootDir>/test/setup.js']
-};
--- jest.config.mjs
+++ jest.config.mjs
@@ -6,7 +6,6 @@ export default {
   coverageReporters: ['text', 'lcov'],
   collectCoverageFrom: ['src/**/*.js'],
   transform: {},
-  extensionsToTreatAsEsm: ['.js'],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1'
   }
